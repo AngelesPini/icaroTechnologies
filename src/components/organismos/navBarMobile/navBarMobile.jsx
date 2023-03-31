13ae1d36622e65fd57a346fe50004597a14ce76d
angelespini <angelesbpini@gmail.com>
2023-03-31 11:30:41 -0300

    responsive, logo, video

--- src/components/organismos/navBarMobile/navBarMobile.jsx
+++ src/components/organismos/navBarMobile/navBarMobile.jsx
@@ -8,25 +8,29 @@ import Menu from '../../atomos/textos/menu';
 import { NavLink } from 'react-router-dom';
 import Logo from '../../../assetes/brand.png'
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+
 const NavBarMobile = () => {
+  const [expanded, setExpanded] = useState(false)
+
   const [t,i18n] = useTranslation("global");
     return (
         <div className='navBarMobile '>
-        <Navbar bg="light" expand="lg">
-        <Container>
+        <Navbar bg="light" expand="lg" expanded={expanded}>
+        <Container >
         <NavLink to='/'><Navbar.Brand href="#home"><img src={Logo} alt="logo de ícaro technologies" width='218' height='50' />
           </Navbar.Brand>               
         </NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
+          <Navbar.Collapse id="basic-navbar-nav" >
             <Nav className="me-auto">
-                <NavLink to='/' className={({isActive}) => isActive ? 'menuActive' : ""}>
-                    <Menu value={t("Navbar.home")} style='menuMedium'/>
+                <NavLink onClick={ ()=> setExpanded(false)} to='/' className={({isActive}) => isActive ? 'menuActive' : ""}>
+                    <Menu value={t("Navbar.home")} style='menuMedium' />
                 </NavLink>
-                <NavLink to='/servicio' className={({isActive}) => isActive ? 'menuActive' : ""}>
+                <NavLink onClick={ ()=> setExpanded(false)} to='/servicio' className={({isActive}) => isActive ? 'menuActive' : ""}>
                     <Menu value={t("Navbar.services")}style='menuMedium'/>
                 </NavLink>
-                <NavLink to='/contacto' className={({isActive}) => isActive ? 'menuActive' : ""}>
+                <NavLink onClick={ ()=> setExpanded(false)} to='/contacto' className={({isActive}) => isActive ? 'menuActive' : ""}>
                     <Menu value={t("Navbar.contact")} style='menuMedium'/>
                 </NavLink>
               <SelectIdiomas/>
